4116824cdd8a806a2ff63143c6aace7b9d1d9306
detextre4 <detextre4@gmail.com>
2023-09-01 02:11:05 -0400

    update typed

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,21 @@
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+import Loader from '@/components/modals/loader.vue'
+import ModalConfirm from '@/components/modals/modal-confirm.vue'
+import VImgLoad from '@/components/v-img-load.vue'
+import VImgInput from '@/components/v-img-input.vue'
+
+declare module '@vue/runtime-core' {
+  export interface GlobalComponents {
+    Loader: typeof Loader,
+    ModalConfirm: typeof ModalConfirm,
+    VImgLoad: typeof VImgLoad,
+    VImgInput: typeof VImgInput,
+  }
 }
+
+// declare module '*.vue' {
+//   import type { DefineComponent } from 'vue'
+//   const component: DefineComponent<{}, {}, any>
+//   export default component
+// }
