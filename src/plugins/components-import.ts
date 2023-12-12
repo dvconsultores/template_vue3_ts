fc007fc73a7dc87f4c7896ba593c64febaf919a7
detextre4 <detextre4@gmail.com>
2023-12-12 17:47:40 -0400

    fixed import error

--- src/plugins/components-import.ts
+++ src/plugins/components-import.ts
@@ -6,17 +6,10 @@ import VImgInput from '@/components/helpers/v-img-input.vue'
 import VAvatarUpload from '@/components/helpers/v-avatar-upload.vue'
 
 // ? imported from external
-import { VSkeletonLoader } from 'vuetify/labs/VSkeletonLoader'
-import { VOtpInput } from 'vuetify/labs/VOtpInput'
-import { VDataTable, VDataTableServer } from 'vuetify/labs/VDataTable'
 import VueApexCharts from "vue3-apexcharts"
 import VueRecaptcha from 'vue3-recaptcha2'
 
 export default {
-  VSkeletonLoader,
-  VOtpInput,
-  VDataTable,
-  VDataTableServer,
   VueApexCharts,
   VueRecaptcha,
   FullScreen,
