946f21d0cbca9064b177335b7181b61b3b87e466
detextre4 <detextre4@gmail.com>
2024-05-18 13:25:11 -0400

    fix/import-labs

--- src/plugins/components-import.ts
+++ src/plugins/components-import.ts
@@ -6,10 +6,16 @@ import VImgInput from '@/components/helpers/v-img-input.vue'
 import VAvatarUpload from '@/components/helpers/v-avatar-upload.vue'
 
 // ? imported from external
+import { VOtpInput, VDataTable, VDataTableServer, VStepper, VDatePicker } from 'vuetify/lib/labs/components.mjs'
 import VueApexCharts from "vue3-apexcharts"
 import VueRecaptcha from 'vue3-recaptcha2'
 
 export default {
+  VOtpInput,
+  VDataTable,
+  VDataTableServer,
+  VStepper,
+  VDatePicker,
   VueApexCharts,
   VueRecaptcha,
   FullScreen,
