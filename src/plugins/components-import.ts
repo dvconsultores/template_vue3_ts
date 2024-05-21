c7af2ae3c054dd6cf5d681edd76423b2cb78e455
detextre4 <detextre4@gmail.com>
2024-05-21 11:50:12 -0400

    fix/updated-vuetify

--- src/plugins/components-import.ts
+++ src/plugins/components-import.ts
@@ -6,17 +6,10 @@ import VImgInput from '@/components/helpers/v-img-input.vue'
 import VAvatarUpload from '@/components/helpers/v-avatar-upload.vue'
 
 // ? imported from external
-import { VOtpInput, VSkeletonLoader, VDataTable, VDataTableServer, VStepper, VDatePicker } from 'vuetify/lib/labs/components.mjs'
 import VueApexCharts from "vue3-apexcharts"
 import VueRecaptcha from 'vue3-recaptcha2'
 
 export default {
-  VOtpInput,
-  VSkeletonLoader,
-  VDataTable,
-  VDataTableServer,
-  VStepper,
-  VDatePicker,
   VueApexCharts,
   VueRecaptcha,
   FullScreen,
