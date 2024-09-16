a1cdc54109a5d963eee7f6b7d6468dc5e6e9dc2e
detextre4 <detextre4@gmail.com>
2024-09-16 13:58:31 -0400

    config/updated

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav id="navbar" class="flex-center">
     <div id="navbar__wrapper">
-      <h6 class="text-center text-white">navbar</h6>
+      <h6 class="text-center">navbar</h6>
     </div>
   </nav>
 </template>
@@ -13,7 +13,6 @@
   animation: movedown .5s $ease-return;
   transition: transform 0.4s ease-in-out;
   @extend .margin-global;
-  @extend .full-screen;
   height: var(--h-navbar);
   position: sticky;
   top: 0;
--- src/layouts/default-layout.vue
+++ src/layouts/default-layout.vue
@@ -2,14 +2,16 @@
   <v-app id="layout">
     <v-main>
       <Drawer></Drawer>
+      <Navbar></Navbar>
       <router-view />
       <Footer></Footer>
     </v-main>
   </v-app>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import '@/assets/styles/layouts/default-layout.scss'
 import Drawer from '@/components/drawer.vue'
+import Navbar from '@/components/navbar.vue'
 import Footer from '@/components/footer.vue'
 </script>
